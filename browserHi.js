cfe19e9c9ceef3739046ba79b2a43fb00857df9e
tsunchen <tsunchen@users.noreply.github.com>
2018-03-17 07:43:22 +0800

    Create browserHi.js

---
+++ browserHi.js
@@ -0,0 +1,8 @@
+var http = require('http');
+http.createServer(function(request, response) {
+    response.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'});
+    console.log('访问');
+    response.write('Hi, Friend');
+    response.end('');
+}).listen(2504);
+console.log('Server running at http://127.0.0.1:2504');
