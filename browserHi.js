274ceabbfd723b2a5d825d19351b47584780afd3
tsunchen <tsunchen@users.noreply.github.com>
2018-03-17 07:53:43 +0800

    Update browserHi.js

--- browserHi.js
+++ browserHi.js
@@ -4,7 +4,13 @@ http.createServer(function(request, response) {
     if (request.url !== "/favicon.ico") {
         console.log('访问');
         response.write('Hi, Friend');
+        func1(response);
         response.end('');
     }
 }).listen(2504);
 console.log('Server running at http://127.0.0.1:2504');
+
+function func1(res){
+    console.log('func1 has run');
+    res.write('running func1 on browser');
+}
