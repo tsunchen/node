fffde870546e8d227c30a137e5fc689c336e8335
tsunchen <tsunchen@users.noreply.github.com>
2018-03-17 08:10:53 +0800

    Update browserHi.js

--- browserHi.js
+++ browserHi.js
@@ -1,10 +1,15 @@
 var http = require('http');
+var func = require("./functions/funcall.js")
+
 http.createServer(function(request, response) {
     response.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'});
     if (request.url !== "/favicon.ico") {
         console.log('访问');
         response.write('Hi, Friend');
-        func1(response);
+        func.foo1(response);
+        func.foo2(response);
+        func['foo2'](response);
+        func['foo1'](response);
         response.end('');
     }
 }).listen(2504);
