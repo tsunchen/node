fd718101ac8134c2b0eab2e26ef602ad2325b8fd
tsunchen <tsunchen@users.noreply.github.com>
2018-03-23 11:55:19 +0800

    Update browserHi_server.js

--- browserHi_server.js
+++ browserHi_server.js
@@ -7,8 +7,10 @@
 var http = require('http');
 var fs = require('fs');
 console.log("Starting up...");
-var host = "127.0.0.1";
-var port = 1333;
+var configs = JSON.parse(fs.readFileSync("./files/configs.json"));
+//var config = JSON.parse(fs.readFileSync("config.json"));
+var host = configs.host;
+var port = configs.port;
 var server = http.createServer(function(request, response){
 
 	console.log("Received request: " + request.url);
@@ -29,6 +31,16 @@ var server = http.createServer(function(request, response){
 });
 
 server.listen(port, host, function(){
-
 	console.log("Listening " + host + ":" + port);
+});
+
+fs.watchFile("./files/configs.json", function(){
+    //console.log("Configs changed");
+    configs = JSON.parse(fs.readFileSync("./files/configs.json"));
+    host = configs.host;
+    port = configs.port;
+    server.close();
+    server.listen(port, host, function(){
+    	console.log("Now listening: " + host + ":" + port);
+    });
 });
