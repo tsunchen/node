b9a352fe4799f35258e19dff10964ceb01292cac
tsunchen <tsunchen@users.noreply.github.com>
2018-03-31 17:15:17 +0800

    Update router.js

--- router.js
+++ router.js
@@ -1,16 +1,33 @@
+var func = require("../functions/funcall");
+
 module.exports = {
 
-	login: function(req, resp) {
+	hello: function(req, resp) {
 
-		resp.write("登录.api");
+		function recall(data) {
 
+			response.write(data);
+			response.end('');
+		}
+
+		//resp.write("登录.api");
+		func.readfile('./public/hello.html', recall);
 
 	},
 
 
 	register: function(req, resp) {
 
-		resp.write("注册.api");
+		function recall(data) {
+
+			response.write(data);
+			response.end('');
+		}
+
+		//resp.write("注册.api");
+		func.readfile('./public/register.html', recall);
+
+
 	}
 
 
