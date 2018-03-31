50a5014faf3e8bbaadeea38e5ccc8e9c79b542f9
tsunchen <tsunchen@users.noreply.github.com>
2018-03-31 17:15:48 +0800

    Update funcall.js

--- funcall.js
+++ funcall.js
@@ -1,22 +1,47 @@
+
+var fs = require('fs');
 // support many functions
 module.exports = {
 
+    readfileSync: function(path){
+
+        var data = fs.readFileSync(path, 'utf-8');
+        console.log(data);
+        console.log("Sync called");
+    },
+
+    readfile: function(path){
+
+        fs.readFile(path, function (err, data) {
+
+            if (err) {
+                console.log(err);
+            } else {
+                console.log(data.toString());
+            }
+        });
+        console.log("ASync called");
+    },
+
     // func3  
-    foo3:function(res){
+    foo3:function(resp){
         console.log('foo3 has run');
-        res.write('running foo3 on browser');
+        resp.write('running foo3 on browser');
     },
 
     // func1
-    foo1:function(res){
-        console.log('foo1 has run');
-        res.write('running foo1 on browser');
+    foo1:function(resp){
+        console.log('foo1 has run运行');
+        resp.write('运行running foo1 on browser');
     },
 
     // func2  
-    foo2:function(res){
+    foo2:function(resp){
         console.log('foo2 has run');
-        res.write('running foo2 on browser');
+        resp.write('running foo2 on browser');
     }
 
 }
+
+// support only one function
+// module.exports = func2;
